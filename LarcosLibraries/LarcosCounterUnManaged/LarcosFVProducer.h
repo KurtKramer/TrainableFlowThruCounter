809cd91d622ab1183025120a74819fc50ae2dbb0
Kurt Kramer <kurtkramer@gmail.com>
2017-12-28 21:32:41 -0800

    Cleaning up g++ warnings.

--- LarcosLibraries/LarcosCounterUnManaged/LarcosFVProducer.h
+++ LarcosLibraries/LarcosCounterUnManaged/LarcosFVProducer.h
@@ -44,10 +44,7 @@ namespace LarcosCounterUnManaged
     virtual  const type_info*  FeatureVectorListTypeId () const;
 
 
-    virtual  FeatureVectorListPtr  ManufacturFeatureVectorList (bool     owner,
-                                                                RunLog&  runLog
-                                                               )
-                                                               const;
+    virtual  FeatureVectorListPtr  ManufacturFeatureVectorList (bool owner)  const;
 
     virtual  kkint16  Version ()  const {return _LarcosFVProducer_VersionNum_;}
 
